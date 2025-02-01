db2601b7abc0d2f0e02b9dd4cc2413b62df5d6c6
YOGESH-RAJ-S <yogesh1470@outlook.com>
2025-02-01 10:53:00 +0530

    Update projects.js

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -77,7 +77,7 @@ const Project = ({ type, title, img, link, github, summary }) => {
         <Link href={link} target="_blank" className="hover:underline underline-offset-2">
           <h2 className="my-2 w-full text-left text-3xl font-bold lg:text-2xl">{title}</h2>
         </Link>
-        <p className="my-2 font-medium text-dark dark:text-light">{summary}</p>
+     <p className="my-2 font-medium text-dark dark:text-light">{summary}</p>
 
         <div className="w-full mt-2 flex items-center justify-between">
         <Link href={link} target="_blank" className=" bg-dark text-light p-2 px-6 
