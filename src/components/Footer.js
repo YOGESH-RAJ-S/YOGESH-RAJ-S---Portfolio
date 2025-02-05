157d32caada6f504df14a9e54a154c4960db2be6
YOGESH-RAJ-S <yogesh1470@outlook.com>
2025-02-05 22:34:37 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,35 +1,45 @@
-import React from 'react'
+import React from 'react';
 import Layout from '../components/Layout';
-import Link from 'next/link';
 
 const Footer = () => {
   return (
-   <footer className='w-full border-t-2 border-solid border-dark font-medium text-lg 
-   dark:text-light dark:border-light sm:text-base'>
-    <Layout className='py-8 flex items-center justify-between lg:flex-col lg:py-6'>
+    <footer className="w-full border-t-2 border-solid border-dark font-medium text-lg 
+      dark:text-light dark:border-light sm:text-base">
+      <Layout className="py-8 flex items-center justify-between lg:flex-col lg:py-6">
+        {/* Copyright Text */}
         <span>
-           {new Date().getFullYear()}&copy; All Rights Reserved.
+          {new Date().getFullYear()}&copy; All Rights Reserved.
         </span>
-        <div className='flex items-center lg:py-2'>
-          Build with 
-          <span className='text-primary dark:text-primaryDark text-2xl px-1'>
-            &#9825;
-          </span> 
-          by&nbsp;
-          <a href="https://wa.me/916369651071" 
-             className='underline underline-offset-2' 
-             target="_blank" rel="noopener noreferrer">
-            Yogesh Raj
-          </a>  
-        </div>
-        <a href="https://wa.me/916369651071"   
-           className='underline underline-offset-2' 
-           target="_blank" rel="noopener noreferrer">
-           Say Hello
-        </a>
-    </Layout>
-   </footer>
-  )
-}
 
-export default Footer
+        {/* Build by Section */}
+        <div className="flex items-center lg:py-2">
+          Build with
+          <span className="text-primary dark:text-primaryDark text-2xl px-1">
+            &#9825;
+          </span>
+          by&nbsp;
+          <a
+            href="https://wa.me/916369651071"
+            className="underline underline-offset-2"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Yogesh Raj
+          </a>
+        </div>
+
+        {/* Say Hello Link */}
+        <a
+          href="https://wa.me/916369651071"
+          className="underline underline-offset-2 text-primary dark:text-primaryDark"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Say Hello
+        </a>
+      </Layout>
+    </footer>
+  );
+};
+
+export default Footer;
